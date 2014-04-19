0c5950870aafaaff1d2935b1bc6727fcc483aaee
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-04-19 22:25:46 +0900

    Initial commit with Caesar cypher.

---
+++ cipher.hs
@@ -0,0 +1,47 @@
+-- Returns index of uppercase char starting from A as 0.
+index :: Char -> Int
+index c = fromEnum c - fromEnum 'A'
+
+-- Number of alphabets.
+atoz :: Int
+atoz = fromEnum 'Z' - fromEnum 'A' + 1
+
+-- Shift char by offset.
+shift :: Int -> Char -> Char
+shift offset c = toEnum $ fromEnum 'A' + fromA
+  where fromA = (index c + offset) `mod` atoz
+
+-- Check if a char is lowercase.
+isLower :: Char -> Bool
+isLower c = 'a' <= c && c <= 'z'
+
+-- Check if a char is uppercase.
+isUpper :: Char -> Bool
+isUpper c = 'A' <= c && c <= 'Z'
+
+-- Lowercase char.
+toLower :: Char -> Char
+toLower c
+  | isUpper c = toEnum $ fromEnum c + 32
+  | otherwise = c
+
+-- Uppercase char.
+toUpper :: Char -> Char
+toUpper c
+  | isLower c = toEnum $ fromEnum c - 32
+  | otherwise = c
+
+-- Ceasar cipher.
+caesar :: Int -> String -> String
+caesar offset str = map (shift offset) original
+  where original = filter isUpper $ map toUpper str
+
+main :: IO ()
+main = do
+  putStrLn "Input words to encrypt:"
+  line <- getLine
+  putStrLn "Input offset to shift:"
+  offsetStr <- getLine
+  let offset = read offsetStr :: Int
+  putStrLn $ caesar offset line
+
