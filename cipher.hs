98b34ad9682a97ee4c95b617208ba2ff2f718e2d
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-04-19 23:25:36 +0900

    Add Vigenere cipher.

--- cipher.hs
+++ cipher.hs
@@ -31,17 +31,36 @@ toUpper c
   | isLower c = toEnum $ fromEnum c - 32
   | otherwise = c
 
+-- Remove non-alphabetic chars and make uppercase.
+normalize :: String -> String
+normalize = filter isUpper . map toUpper
+
 -- Ceasar cipher.
 caesar :: Int -> String -> String
-caesar offset str = map (shift offset) original
-  where original = filter isUpper $ map toUpper str
+caesar offset = map (shift offset) . normalize
+
+-- Offset for the char in the key at given index.
+offsetForIndex :: String -> Int -> Int
+offsetForIndex key idx = index $ key !! i
+  where i = idx `mod` (length key)
+
+-- Vigenere cipher.
+vigenere :: String -> String -> String
+vigenere key plain = zipWith (\ c i -> shift (offsetForIndex normKey i) c) normPlain [0..]
+  where normKey = normalize key
+        normPlain = normalize plain
 
 main :: IO ()
 main = do
   putStrLn "Input words to encrypt:"
-  line <- getLine
-  putStrLn "Input offset to shift:"
+  words <- getLine
+
+  putStrLn "Input offset for Ceasar cipher:"
   offsetStr <- getLine
   let offset = read offsetStr :: Int
-  putStrLn $ caesar offset line
+  putStrLn $ caesar offset words
+
+  putStrLn "Input key for Vigenere cipher:"
+  key <- getLine
+  putStrLn $ vigenere key words
 
